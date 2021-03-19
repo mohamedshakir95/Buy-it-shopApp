aa05095948874e2740ec24dbcfb187f0ffb0cca9
mohamedshakir95 <mohamed.shakir.business@gmail.com>
2021-03-19 19:41:33 +0300

    second one

--- lib/screens/edit_product.dart
+++ lib/screens/edit_product.dart
@@ -13,60 +13,103 @@ class EditProduct extends StatefulWidget {
 }
 
 class _EditProductState extends State<EditProduct> {
-  
+  final _store = Store();
   @override
   Widget build(BuildContext context) {
-    
-    List _products = [];
-    dynamic result;
-    fechProductsList() async {
-       result = await Store().loadProducts();
-       
-        
-print('length = ${result.length}');
-      
-      if (result == null) {
-        print('unable to retrieve');
-      } else {
-        setState(() {
-          _products = result;
-        });
-      }   
-   
-    }
-    
-     
+//     List _products = [];
+//     dynamic result;
+//     fechProductsList() async {
+//        result = await Store().loadProducts();
 
-    @override
-    void initState() {
-      super.initState();
-      fechProductsList();
-      
-    }
-    
- print('length products = ${_products.length}');
- 
+// print('length = ${result.length}');
+
+//       if (result == null) {
+//         print('unable to retrieve');
+//       } else {
+//         setState(() {
+//           _products = result;
+//         });
+//       }
+
+//     }
+
+//     @override
+//     void initState() {
+//       super.initState();
+//       fechProductsList();
+
+//     }
+
+//  print('length products = ${_products.length}');
 
     return Scaffold(
       appBar: AppBar(
-        title:Text('kk'),
+        title: Text('kk'),
       ),
-      
-      body: Center(
-        child: Container(
-          
-             
-            child: ListView.builder(
-              itemCount: _products.length,
-              itemBuilder: (context, index) =>
-               Text(_products[index].kProductName),
-              
-               
-            ),
-            
-          ),
-      ),
+      backgroundColor: kMainColor,
+      body: StreamBuilder<QuerySnapshot>(
+          stream: _store.loadProducts(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<Product> products = [];
+              for (var doc in snapshot.data.docs) {
+                var data = doc.data;
+                products.add(Product(
+                    pName: kProductName,
+                    pPrice: kProductPrice,
+                    pDescription: kProductDescription,
+                    pLocation: kProductLocation,
+                    pCategory: kProductCategory));
+              }
+              return GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                    itemCount: products.length,
+                itemBuilder: (context, index) => Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: Stack(
+                    children: <Widget>[
+                       Text(products[index].pName),
+                      // Positioned.fill(
+                      //   child: Image(
+                      //     fit: BoxFit.fill,
+                      //     image: AssetImage(products[index].pLocation),
+                      //   ),
+                      // ),
+                      
+                    ],
+                  ),
+                ),
+              );
+            }else {
+              return Center(child: Text('Loading ...'));
+            }
+          }),
     );
-    
   }
 }
+
+// body: StreamBuilder(
+//     stream: FirebaseFirestore.instance
+//         .collection(kProductsCollection)
+//         .snapshots(),
+//     builder: (context, snapshot) {
+//       return ListView.builder(
+//           itemCount: snapshot.data.docs.length,
+//           itemBuilder: (context, index) {
+//             DocumentSnapshot product = snapshot.data.docs[index];
+//             return Column(
+//               children: <Widget>[
+//                 ListTile(
+//                   title: Text(product[kProductName].toString()),
+//                   subtitle: Text(product[kProductPrice].toString()),
+//                 ),
+//                 //           CustomTextField(
+//                 //            onClick: (Value) {
+//                 //           var _price = Value;
+//                 // },),
+//                 //Text((product[kProductName].toString()),)
+//               ],
+//             );
+//           });
+//     }),
--- lib/services/store.dart
+++ lib/services/store.dart
@@ -1,19 +1,18 @@
 import 'package:buy_it_shop/model/product.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:buy_it_shop/constant.dart';
-import 'package:meta/meta.dart';
  class Store {
   final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
   //CollectionReference users = FirebaseFirestore.instance.collection(kProductsCollection);
-  dynamic list;
-  CollectionReference productsList = FirebaseFirestore.instance
-    .collection(kProductsCollection);
+  //dynamic list;
+  //CollectionReference productsList = FirebaseFirestore.instance
+   // .collection(kProductsCollection);
 
     
 
   addProduct(Product product)
    {
-    list = _fireStore.collection(kProductsCollection).add(
+   _fireStore.collection(kProductsCollection).add(
        {
          kProductName : product.pName,
          kProductPrice : product.pPrice,
@@ -22,7 +21,9 @@ import 'package:meta/meta.dart';
          kProductCategory : product.pCategory
        }
      );
-     print(list.length);
+   }
+   Stream<QuerySnapshot> loadProducts(){
+     return _fireStore.collection(kProductsCollection).snapshots();
    }
    // Product product;
     // Future<void> addProduct() async{
@@ -36,21 +37,21 @@ import 'package:meta/meta.dart';
     // }
 
 
-  Future<List<Product>>  loadProducts() async
-  {
-    var snapshot = await  _fireStore..collection(kProductsCollection).getDocuments();
-    List<Product> products = [];
-    for(var doc in snapshot.)
-    {
-      var data = doc.data;
-       products.add(Product(
-         pName: kProductCategory ,
-           pPrice  :kProductPrice,
-          pDescription : kProductDescription,
-          pLocation : kProductLocation,
-         pCategory : kProductCategory
+  // Future<List<Product>>  loadProducts() async
+  // {
+  //   var list = await  _fireStore..collection(kProductsCollection).snapshots();
+    // List<Product> products = [];
+    // for(var doc in list.())
+    // {
+      // var data = doc.data;
+      //  products.add(Product(
+      //    pName: kProductCategory ,
+      //      pPrice  :kProductPrice,
+      //     pDescription : kProductDescription,
+      //     pLocation : kProductLocation,
+      //    pCategory : kProductCategory
 
-         ));
+      //    ));
 
      // QuerySnapshot snapShot = await _fireStore.collection(kProductsCollection).get();
     //List itemList = []; 
@@ -72,9 +73,9 @@ import 'package:meta/meta.dart';
     // }
     
     
-  }
+  // }
 
-  }
+  // }
 }
    
     
